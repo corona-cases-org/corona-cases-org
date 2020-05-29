087de6addf67cf707e7a54561d96a0abb20ed90e
Jo Liss <joliss42@gmail.com>
2020-05-29 14:19:03 +0000

    Fetch all COVID charts from Wikipedia and save data to data.json

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-let wtf = require('wtf_wikipedia');
-let moment = require('moment');
-let evaluateExpr = require('./_expr');
+let fs = require('fs')
+
+let fetch = require('node-fetch')
+let wtf = require('wtf_wikipedia')
+let moment = require('moment')
+let evaluateExpr = require('./_expr')
 
 class DataPoint {
   constructor(date, deaths, recoveries, totalCases) {
@@ -24,43 +27,76 @@ function printDataSet(dataSet) {
   console.log('')
 }
 
-let pages = process.argv.slice(2);
-if (pages.length === 0) {
-  pages = ['Template:COVID-19_pandemic_data/United_Kingdom_medical_cases_chart'];
+async function findCharts() {
+
 }
 
 async function main() {
-  for (let i = 0; i < pages.length; i++) {
-    console.time('fetch')
-    let doc = await wtf.fetch(pages[i], 'en');
-    console.timeEnd('fetch')
-    let chart = doc.template('Medical cases chart');
-    // console.log(chart)
-    // console.log(chart.data[34])
-    //chart.recoveries (no|n|0)
-    let dataSet = {};
-    dataSet.casesLabel = chart.altlbl1 || 'Cases';
-    dataSet.recoveriesLabel = chart.reclbl || 'Recoveries';
-    if (chart.altlbl2 != null || chart.altlbl3 != null) {
-      throw new Error('Unexpected extra label');
+  let pages = process.argv.slice(2);
+  if (pages.length === 0) {
+    let rawResponse = await fetch('https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=intitle:chart%20prefix:Template:COVID-19_pandemic_data/&utf8=&format=json&srlimit=500&srprop=')
+    let response = await rawResponse.json()
+    if (response.continue) {
+      console.error(response)
+      throw new Error('paging not implemented')
     }
-    dataSet.points = []
-    for (let j = 0; j < chart.data.length; j++) {
-      let row = chart.data[j];
-      dataSet.points.push(new DataPoint(
-        moment(row.date, 'YYYY-MM-DD'),
-        evaluateExpr(row.deathsExpr),
-        evaluateExpr(row.recoveriesExpr),
-        evaluateExpr(row.casesExpr)
-      ))
-    }
-    console.log('===', pages[i], '===')
-    printDataSet(dataSet);
-
-    // console.log(dataSet.points)
-    // console.log(dataSet.points[34])
-    // debugger
+    pages = response.query.search.map((item) => item.title).filter((title) => !title.match('/doc'))
   }
+
+  console.time('fetch')
+  let docs = [];
+  let strideLength = 50;
+  for (let i = 0; i < pages.length; i += strideLength) {
+    console.log('fetching', i, '-', i + strideLength - 1)
+    let pageSubset = pages.slice(i, i + strideLength)
+
+    let docsSubset = await wtf.fetch(pageSubset, 'en')
+    // let docsSubset = [ null, null ]
+    if (docsSubset == null) {
+      console.error(pageSubset)
+      throw new Error('did not receive pages')
+    }
+    if (!Array.isArray(docsSubset)) docsSubset = [docsSubset]
+    docs = docs.concat(docsSubset)
+    await new Promise(r => setTimeout(r, 2000));
+  }
+  console.timeEnd('fetch')
+  let wikipediaPages = []
+  for (let i = 0; i < docs.length; i++) {
+    let doc = docs[i]
+    let wikipediaPage = {
+      url: doc.url(),
+      title: doc.title()
+    }
+    try {
+      let chart = doc.template('Medical cases chart');
+      if (chart == null) {
+        throw new Error('Medical_cases_chart not found')
+      }
+      //chart.recoveries (no|n|0)
+      let dataSet = {};
+      dataSet.casesLabel = chart.altlbl1 || 'Cases';
+      dataSet.recoveriesLabel = chart.reclbl || 'Recoveries';
+      if (chart.altlbl2 != null || chart.altlbl3 != null) {
+        throw new Error('Unexpected extra label');
+      }
+      dataSet.points = []
+      for (let j = 0; j < chart.data.length; j++) {
+        let row = chart.data[j];
+        dataSet.points.push(new DataPoint(
+          moment(row.date, 'YYYY-MM-DD'),
+          evaluateExpr(row.deathsExpr),
+          evaluateExpr(row.recoveriesExpr),
+          evaluateExpr(row.casesExpr)
+        ))
+      }
+      wikipediaPage.ok = dataSet
+    } catch (err) {
+      wikipediaPage.err = err + ''
+    }
+    wikipediaPages.push(wikipediaPage)
+  }
+  fs.writeFileSync('data.json', JSON.stringify(wikipediaPages))
 }
 
 main().then(() => {
