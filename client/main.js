841f4fd188085168dade658a22d7aed9d1ab54f4
Jo Liss <joliss42@gmail.com>
2020-06-05 15:31:51 +0000

    Reformat

--- client/main.js
+++ client/main.js
@@ -6,76 +6,76 @@ import "highcharts/modules/accessibility"
 import moment from "moment/dist/moment"
 
 $(() => {
-    let { region, regionData } = window.backendData;
-    let dataSet = regionData.dataSet;
-    let caseSeries = []
-    for (let i = 0; i < dataSet.points.length; i++) {
-        let point = dataSet.points[i];
-        let date = moment(point.date)
-        if (point.totalCases != null) {
-            caseSeries.push([Date.UTC(date.year(), date.month(), date.date()), point.totalCases])
-        }
+  let { region, regionData } = window.backendData;
+  let dataSet = regionData.dataSet;
+  let caseSeries = []
+  for (let i = 0; i < dataSet.points.length; i++) {
+    let point = dataSet.points[i];
+    let date = moment(point.date)
+    if (point.totalCases != null) {
+      caseSeries.push([Date.UTC(date.year(), date.month(), date.date()), point.totalCases])
     }
+  }
 
-    Highcharts.chart('container', {
-        chart: {
-            type: 'area'
-        },
-        title: {
-            text: undefined
-        },
-        xAxis: {
-            type: 'datetime',
-            dateTimeLabelFormats: { // don't display the dummy year
-                month: '%e. %b',
-                year: '%b'
-            },
-            title: {
-                text: 'Date'
-            }
-        },
-        yAxis: {
-            title: {
-                text: undefined
-            },
-            floor: 0,
-            type: 'linear',
-        },
-        tooltip: {
-        },
+  Highcharts.chart('container', {
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: undefined
+    },
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: { // don't display the dummy year
+        month: '%e. %b',
+        year: '%b'
+      },
+      title: {
+        text: 'Date'
+      }
+    },
+    yAxis: {
+      title: {
+        text: undefined
+      },
+      floor: 0,
+      type: 'linear',
+    },
+    tooltip: {
+    },
 
-        plotOptions: {
+    plotOptions: {
+      series: {
+        marker: {
+          enabled: false
+        },
+        label: false,
+        // step: 'left',
+      }
+    },
+
+    colors: [/*'#6CF'*/ '#F88', '#39F', '#06C', '#036', '#000'],
+
+    series: [{
+      name: "Confirmed cases",
+      data: caseSeries
+    }],
+
+    responsive: {
+      rules: [{
+        condition: {
+          maxWidth: 500
+        },
+        chartOptions: {
+          plotOptions: {
             series: {
-                marker: {
-                    enabled: false
-                },
-                label: false,
-                // step: 'left',
+              marker: {
+                radius: 2.5
+              }
             }
-        },
-
-        colors: [/*'#6CF'*/ '#F88', '#39F', '#06C', '#036', '#000'],
-
-        series: [{
-            name: "Confirmed cases",
-            data: caseSeries
-        }],
-
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    plotOptions: {
-                        series: {
-                            marker: {
-                                radius: 2.5
-                            }
-                        }
-                    }
-                }
-            }]
+          }
         }
-    });
+      }]
+    }
+  });
 })
--- start/hooks.js
+++ start/hooks.js
@@ -1,7 +1,7 @@
 const { hooks } = require('@adonisjs/ignitor')
 
 hooks.after.providersRegistered(() => {
-    const View = use('View')
+  const View = use('View')
 
-    View.global('NODE_ENV', use('Env').get('NODE_ENV'))
+  View.global('NODE_ENV', use('Env').get('NODE_ENV'))
 })
