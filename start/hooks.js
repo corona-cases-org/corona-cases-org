14be231cf50525caf3c8bfbc29b779e7036449b0
Jo Liss <joliss42@gmail.com>
2020-06-18 12:29:54 +0000

    Report SOURCE_VERSION (Git commit) to Sentry

--- start/hooks.js
+++ start/hooks.js
@@ -4,4 +4,5 @@ hooks.after.providersRegistered(() => {
   const View = use('View')
 
   View.global('NODE_ENV', use('Env').get('NODE_ENV'))
+  View.global('SOURCE_VERSION', process.env.SOURCE_VERSION || 'missing')
 })
