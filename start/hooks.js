447d8dd190df6c48aaf1fe17f1dc3d0a2083a9f8
Jo Liss <joliss42@gmail.com>
2020-08-13 14:42:29 +0000

    Use JavaScript-free social sharing buttons
    Inspired by https://cloudcannon.com/tutorials/2017/02/01/lightweight-social-share-buttons/
    
    The JavaScript widgets are massive resource hogs and slow down initial
    page load times.

--- start/hooks.js
+++ start/hooks.js
@@ -6,6 +6,7 @@ hooks.after.providersRegistered(() => {
   View.global('NODE_ENV', use('Env').get('NODE_ENV'))
   View.global('SOURCE_VERSION', process.env.SOURCE_VERSION || 'missing')
 
+  View.global('encodeURIComponent', encodeURIComponent)
   View.global('formatNumber', (n) =>
     n == null ? 'null' : n.toLocaleString('en-US')
   )
